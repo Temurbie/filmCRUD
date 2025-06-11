2715de62a720be26ba943ca257d540c7c92169f3
Temurbie <temurbie@gmail.com>
2025-06-11 17:14:30 +0500

    Generator Film

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <div>
+        <p >hello World</p>
+    </div>
+    <div>
+        <p>test</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        
+    }
+</script>
+
+<style></style>
